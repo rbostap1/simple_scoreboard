43bec400c9ad78956c59ece64e0a97da6705aded
Ryan Bostaph <rbostap1@kent.edu>
2026-01-01 23:28:04 +0000

    Enhance creator join message functionality: add configurable message sender and streamline message broadcasting

--- config.lua
+++ config.lua
@@ -40,6 +40,7 @@ Config.HudColors = {
 -- 🎉 Creator Join Message
 Config.EnableCreatorMessage = true    -- Enable/disable creator join message
 Config.CreatorIdentifier = "license:YOUR_LICENSE_HERE"  -- Your license identifier
+Config.CreatorMessageSender = "^5simple_scoreboard^0"  -- Name that appears before the message (supports color codes)
 Config.CreatorMessage = "^3The script creator ^2Ryan Bostaph ^3has joined the server!^0"  -- Message to display
 
 -----------------------------------
